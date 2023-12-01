2cc7d7844f329f35907f6381b68e6684d31cda3d
bzoviimaxim@gmail.com <bzoviimaxim@gmail.com>
2023-12-01 19:45:34 +0200

    [master] set background to black

--- src/app/features/app-home/app-home.component.ts
+++ src/app/features/app-home/app-home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-app-home',
   templateUrl: './app-home.component.html',
-  styleUrls: ['./app-home.component.css']
+  styleUrls: ['./app-home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppHomeComponent implements OnInit {
 

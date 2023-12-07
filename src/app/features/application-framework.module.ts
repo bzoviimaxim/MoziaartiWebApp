a70df97308f1da5c67d208b73dd0819fc84325dc
bzoviimaxim@gmail.com <bzoviimaxim@gmail.com>
2023-12-07 22:42:56 +0200

    [master] added header comp

--- src/app/features/application-framework.module.ts
+++ src/app/features/application-framework.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppHomeComponent } from './app-home/app-home.component';
-
-
+import { HeaderAreaComponent } from './header-area/header-area.component';
 
 @NgModule({
-  declarations: [AppHomeComponent],
+  declarations: [AppHomeComponent, HeaderAreaComponent],
   imports: [
     CommonModule,
   ],
-  exports:[AppHomeComponent]
+  exports:[AppHomeComponent,HeaderAreaComponent]
 })
 export class ApplicationFrameworkModule { }

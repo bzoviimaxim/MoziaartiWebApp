cadda9b49821be5ac9ca7792fd4b7d4f4903bd27
bzoviimaxim@gmail.com <bzoviimaxim@gmail.com>
2023-12-13 22:29:32 +0200

    [master] automatic deploiment

--- src/app/features/application-framework.module.ts
+++ src/app/features/application-framework.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppHomeComponent } from './app-home/app-home.component';
 import { HeaderAreaComponent } from './header-area/header-area.component';
-
+import {ButtonModule} from 'primeng/button';
 @NgModule({
   declarations: [AppHomeComponent, HeaderAreaComponent],
   imports: [
     CommonModule,
+    ButtonModule
   ],
   exports:[AppHomeComponent,HeaderAreaComponent]
 })
